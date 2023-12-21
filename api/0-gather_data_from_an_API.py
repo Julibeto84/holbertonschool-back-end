84d6172022a2208176268c3e3afe7132709495a9
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2023-12-21 11:42:06 -0500

    0-gather_data_from_an_API.py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+from requests import get
+from json import loads
+
+
+#the required first parameter of the 'get' method is the 'url':
+response = get('https://jsonplaceholder.typicode.com/todos')
+
+todos = loads (response.text)
+todos_done = list(filter(lambda todo: todo['completed'], todos))
+users_tasks = dict()
+
+for todo in todos:
+    user_id = todo['userId']
+    if not user_id in users_tasks:
+        users_tasks[user_id] = list()     
+    user_title = todo['title']
+    users_tasks[user_id].append(user_title)
+    
+#print the response text (the content of the requested file):
+print(users_tasks)
