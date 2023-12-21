4b6abc0329a4d67b74a74a25de4e4fcaf823b709
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2023-12-21 17:53:22 -0500

    update0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,14 +2,19 @@
 """ Getting my first apis """
 
 from json import loads
-from requests import get
 import sys
+from requests import get
+
 
 
 def get_employee_todo_progress(employee_id):
-    # Get TODO list from the API
-    response = get(f'https://jsonplaceholder.typicode.com/todos?userId={employee_id}')
-    todos = loads(response.text)
+    # Fetch TODO list from the API
+    todos_response = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
+    todos = loads(todos_response.text)
+
+    # Fetch user name from the API
+    users_response = get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
+    user = loads(users_response.text)
 
     # Filter completed tasks
     todos_done = list(filter(lambda todo: todo['completed'], todos))
@@ -18,24 +23,18 @@ def get_employee_todo_progress(employee_id):
     number_of_done_tasks = len(todos_done)
     total_number_of_tasks = len(todos)
 
-    # Fetch user name
-    users_response = get('https://jsonplaceholder.typicode.com/users').json()
-    employee_name = None
-    for user in users_response:
-        if user['id'] == employee_id:
-            employee_name = user['name']
-            break
-
+    
     # Display the progress
+    employee_name = user.get('name', 'Unknown')
     print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
 
     # Display completed tasks
     for todo in todos_done:
-        print(f"\t{todo['title']}")
+        print(f"\t {todo['title']}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
+    if len(sys.argv) < 2:
+        print("Usage: script.py <employee_id>")
         sys.exit(1)
 
     try:
