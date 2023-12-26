e7adfea40c3a1574c37a67222b8edb05d09f760d
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2023-12-26 10:29:04 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,13 +9,13 @@ import sys
 def get_employee_todo_progress(employee_id):
 
     # Fetch TODO list from the API
-    todos_response = get('https://jsonplaceholder.typicode.com/todos?userId={}'
-    .format(employee_id))
+    todos_response = get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(employee_id))
     todos = loads(todos_response.text)
 
     # Fetch user name from the API
-    users_response = get('https://jsonplaceholder.typicode.com/users/{}'
-    .format(employee_id))
+    users_response = get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
     user = loads(users_response.text)
 
     # Filter completed tasks
@@ -25,6 +25,7 @@ def get_employee_todo_progress(employee_id):
     number_of_done_tasks = len(todos_done)
     total_number_of_tasks = len(todos)
 
+    
     # Display the progress
     employee_name = user.get('name', 'Unknown')
     print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_number_of_tasks}):")
