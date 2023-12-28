ccc58814702964491c6788b3b81786ad43263d20
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2023-12-28 18:57:04 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -14,22 +14,35 @@ if __name__ == "__main__":
         print("Error: Employee ID must be an integer.")
         sys.exit(1)
 
-    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(employee_id))
-    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id))
+    # Obtener la respuesta de la API para el usuario
+    user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}')
+    # Obtener la respuesta de la API para los todos
+    todos_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos')
 
+    # Convertir la respuesta a JSON
     user = user_response.json()
     todos = todos_response.json()
 
+    # Verificar si se obtuvieron datos válidos
     if not user or not todos:
         print("Error: No data found for the provided employee ID.")
         sys.exit(1)
 
+    # Depuración: imprimir la respuesta de la API directamente
+    print("User API Response:", user_response.text)
+    print("Todos API Response:", todos_response.text)
+
+    # Filtrar tareas completadas
     completed = [todo["title"] for todo in todos if todo.get("completed")]
 
-    # Asegurarse de que el nombre del empleado no tenga espacios adicionales
+    # Extraer y depurar el nombre del empleado
     employee_name = user.get("name").strip()
+    print("Employee Name:", employee_name)  # Depuración
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_name, len(completed), len(todos)))
+    # Imprimir el progreso del empleado
+    print(f"Employee {employee_name} is done with tasks({len(completed)}/{len(todos)}):")
 
+    # Imprimir tareas completadas
     for task in completed:
-        print("\t {}".format(task))
+        print(f"\t {task}")
+
