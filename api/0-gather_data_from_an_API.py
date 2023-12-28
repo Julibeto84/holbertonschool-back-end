d2d84865346f6e4fa7a21615864b4a83afaaa966
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2023-12-28 18:40:17 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,12 +4,32 @@ import requests
 import sys
 
 if __name__ == "__main__":
-        user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
-                        format(argv[1]))
-        tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
-                         format(argv[1]))
+    if len(sys.argv) != 2:
+        print("Usage: script.py <employee_id>")
+        sys.exit(1)
 
-        completed = [t.get("title") for t in todos if t.get("completed") is True]
-        print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-        [print("\t {}".format(c)) for c in completed]
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: Employee ID must be an integer.")
+        sys.exit(1)
+
+    user_response = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                                 .format(employee_id))
+    todos_response = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                                  .format(employee_id))
+
+    user = user_response.json()
+    todos = todos_response.json()
+
+    if not user or not todos:
+        print("Error: No data found for the provided employee ID.")
+        sys.exit(1)
+
+    completed = [todo["title"] for todo in todos if todo.get("completed")]
+
+    print("Employee {} is done with tasks({}/{}):".format(
+    user.get("name"), len(completed), len(todos)))
+
+    for task in completed:
+        print("\t {}".format(task))
