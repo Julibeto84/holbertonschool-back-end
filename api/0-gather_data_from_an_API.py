962d127686b3b31ff7f781051dd13c611519121d
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2023-12-28 18:05:57 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-""" Getting my first apis """
-import json
-import requests
-import sys
+'''for a given employee ID, returns information
+   about his/her TODO list progress.'''
 
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-def format_employee_name(name, max_length=18):
-    """Format employee name with truncation if necessary."""
-    return (name[:max_length - 3] + '...') if len(name) > max_length else name
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(argv[1]))
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
+                         format(argv[1]))
+    done_list = []
+    done_tasks = 0
+    total_tasks = 0
 
+    employee_name = user.json()['name']
+    for task in tasks.json():
+        total_tasks += 1
+        if task['completed'] is True:
+            done_list.append(task['title'])
+            done_tasks += 1
 
-if __name__ == "__main__":
-    """Get API"""
-    todos_api = requests.get(
-        'https://jsonplaceholder.typicode.com/todos/')
-    user_api = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(sys.argv[1]))
-    todo_data = todos_api.text
-    user_data = user_api.text
-    user = json.loads(user_data)
-    todos = json.loads(todo_data)
-    completed = []
-    all_todos = 0
-    for todo in todos:
-        if todo['userId'] == user['id']:
-            if todo['completed']:
-                completed.append(todo)
-            all_todos += 1
-    
-    formatted_name = format_employee_name(user['name'])
-    print(
-        'Employee {} is done with tasks({}/{}):'
-        .format(formatted_name, len(completed), all_todos), file=sys.stdout)
-    
-    for finished_todo in completed:
-        print('\t {}'.format(finished_todo['title']), file=sys.stdout)
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, done_tasks, total_tasks))
+    for task in done_list:
+        print("\t {}".format(task))
