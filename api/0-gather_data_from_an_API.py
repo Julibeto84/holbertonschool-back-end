3ab97cc0cd364e819f9043b2a0df99663932c7a2
Julibeto84 <julibeto84@DESKTOP-D0GC1J4>
2023-12-29 18:30:20 -0500

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,62 +1,27 @@
 #!/usr/bin/python3
-"""
-given employee ID, returns information about his/her todo list progress.
-"""
-import requests
-import sys
+'''for a given employee ID, returns information
+   about his/her TODO list progress.'''
 
+if __name__ == '__main__':
+    import requests
+    from sys import argv
 
-def display_employee():
-    """
-    given employee ID, returns information about his/her todo list progress.
-    """
-    employee_id = sys.argv[1]
-    todos_url = 'https://jsonplaceholder.typicode.com/todos'
-    users_url = 'https://jsonplaceholder.typicode.com/users'
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(argv[1]))
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
+                         format(argv[1]))
+    done_list = []
+    done_tasks = 0
+    total_tasks = 0
 
-    todos = requests.get(todos_url)
-    users = requests.get(users_url)
+    employee_name = user.json()['name']
+    for task in tasks.json():
+        total_tasks += 1
+        if task['completed'] is True:
+            done_list.append(task['title'])
+            done_tasks += 1
 
-    if todos.status_code == 200 and users.status_code == 200:
-        todos_data = todos.json()
-        users_data = users.json()
-
-        # Create a dictionary for mapping user IDs to names
-        user_names = {user['id']: user['name'] for user in users_data}
-
-        completed_tasks = []
-
-        # Get the completed tasks for the specific user
-        for task in todos_data:
-            user_id = task['userId']
-            completed = task['completed']
-            title = task['title']
-
-            if user_id == int(employee_id) and completed:
-                completed_tasks.append(title)
-
-        # Print the employee's completed tasks
-        if int(employee_id) in user_names:
-            EMPLOYEE_NAME = user_names[int(employee_id)]
-            NUMBER_OF_DONE_TASKS = len(completed_tasks)
-            TOTAL_NUMBER_OF_TASKS = len([
-                                         task for task in todos_data if task
-                                         ['userId'] == int(employee_id)
-                                         ])
-
-            print(f"Employee {EMPLOYEE_NAME} is done with tasks"
-                  f"({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):")
-
-            # Print completed tasks in a list
-            for TASK_TITLE in completed_tasks:
-                print(f"\t {TASK_TITLE}")
-        else:
-            print("No employee was found with that ID.")
-
-    else:
-        print("The information could not be obtained from the server.")
-
-
-if __name__ == "__main__":
-
-    display_employee()
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, done_tasks, total_tasks))
+    for task in done_list:
+        print("\t {}".format(task))
