0f108651194dce88b38e7f3a9e03a51dc18eda91
Holberton iMac 22 <holbertonimac22@Holbertons-iMac.local>
2024-01-19 10:39:29 -0500

    reload 0-gather_data_from_an_API.py

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''for a given employee ID, returns information
+   about his/her TODO list progress.'''
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(argv[1]))
+    tasks = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'.
+                         format(argv[1]))
+    done_list = []
+    done_tasks = 0
+    total_tasks = 0
+
+    employee_name = user.json()['name']
+    for task in tasks.json():
+        total_tasks += 1
+        if task['completed'] is True:
+            done_list.append(task['title'])
+            done_tasks += 1
+
+    print("Employee {} is done with tasks({}/{}):".
+          format(employee_name, done_tasks, total_tasks))
+    for task in done_list:
+        print("\t {}".format(task))
